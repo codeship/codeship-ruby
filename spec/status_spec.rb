e0402f7fd1473b99db46f5cddeb1233e84c0b5c6
David Casinghino <davidcasinghino@gmail.com>
2015-11-07 01:13:42 -0500

    Update Content-Disposition response header in stubs from .png to .gif to reflect the format currently returned by codeship api.  Specs with gifs now fail.

--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -2,28 +2,59 @@ require 'spec_helper'
 
 RSpec.describe Codeship::Status do
   context 'parsing the project status' do
-    Codeship::Status::STATES.each do |state|
-      it "should parse #{state}" do
+    context 'of a project with completed build' do
+      Codeship::Status::STATES.each do |state|
+        it "should parse #{state}" do
 
-        stub_request(:head, "https://codeship.com/projects/#{state}/status").
-                 with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-                 to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.png\""})
+          stub_request(:head, "https://codeship.com/projects/#{state}/status").
+                   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+                   to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.png\""})
 
-        project_status = Codeship::Status.new(state)
-        expect(project_status.status).to eq(state)
+          project_status = Codeship::Status.new(state)
+          expect(project_status.status).to eq(state)
+        end
+      end
+    end
+
+    context 'of a project with active build' do
+      Codeship::Status::STATES.each do |state|
+        it "should parse #{state}" do
+
+          stub_request(:head, "https://codeship.com/projects/#{state}/status").
+                   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+                   to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.gif\""})
+
+          project_status = Codeship::Status.new(state)
+          expect(project_status.status).to eq(state)
+        end
       end
     end
   end
 
   context 'parsing the project status on a certain branch' do
-    Codeship::Status::STATES.each do |state|
-      it "should parse #{state}" do
-        stub_request(:head, "https://codeship.com/projects/#{state}/status?branch=master").
-                 with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-                 to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.png\""})
+    context 'of a project with completed build' do
+      Codeship::Status::STATES.each do |state|
+        it "should parse #{state}" do
+          stub_request(:head, "https://codeship.com/projects/#{state}/status?branch=master").
+                   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+                   to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.png\""})
 
-        project_status = Codeship::Status.new(state, branch: 'master')
-        expect(project_status.status).to eq(state)
+          project_status = Codeship::Status.new(state, branch: 'master')
+          expect(project_status.status).to eq(state)
+        end
+      end
+    end
+
+    context 'of a project with active build' do
+      Codeship::Status::STATES.each do |state|
+        it "should parse #{state}" do
+          stub_request(:head, "https://codeship.com/projects/#{state}/status?branch=master").
+                   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+                   to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.gif\""})
+
+          project_status = Codeship::Status.new(state, branch: 'master')
+          expect(project_status.status).to eq(state)
+        end
       end
     end
   end
