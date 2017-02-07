b51764bf5482f7c2bdc1d4fefb900ef5485329f5
Dave Allie <dave@daveallie.com>
2017-02-07 15:02:58 +1000

    Update codeship base url

--- lib/codeship/request.rb
+++ lib/codeship/request.rb
@@ -4,7 +4,7 @@ module Codeship
   module Request
 
     def http_request
-      http = Net::HTTP.new "codeship.com", 443
+      http = Net::HTTP.new "app.codeship.com", 443
       http.use_ssl = true
       http
     end
