80766538276205e7955bb522bbe1050fa0e14d61
Dave Allie <dave@daveallie.com>
2018-04-24 11:46:37 +1000

    Support SVG status image (#14)
    * Support SVG status image
    
    * Reword status test contexts and add svg status tests

--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Codeship::Status do
   context 'parsing the project status' do
-    context 'of a project with completed build' do
+    context 'which returns a png' do
       Codeship::Status::STATES.each do |state|
         it "should parse #{state}" do
 
@@ -16,7 +16,7 @@ RSpec.describe Codeship::Status do
       end
     end
 
-    context 'of a project with active build' do
+    context 'which returns a gif' do
       Codeship::Status::STATES.each do |state|
         it "should parse #{state}" do
 
@@ -29,12 +29,27 @@ RSpec.describe Codeship::Status do
         end
       end
     end
+
+    context 'which returns a svg' do
+      Codeship::Status::STATES.each do |state|
+        it "should parse #{state}" do
+
+          stub_request(:head, "https://app.codeship.com/projects/#{state}/status").
+                   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+                   to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.svg\""})
+
+          project_status = Codeship::Status.new(state)
+          expect(project_status.status).to eq(state)
+        end
+      end
+    end
   end
 
   context 'parsing the project status on a certain branch' do
-    context 'of a project with completed build' do
+    context 'which returns a png' do
       Codeship::Status::STATES.each do |state|
         it "should parse #{state}" do
+
           stub_request(:head, "https://app.codeship.com/projects/#{state}/status?branch=master").
                    with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
                    to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.png\""})
@@ -45,12 +60,27 @@ RSpec.describe Codeship::Status do
       end
     end
 
-    context 'of a project with active build' do
+    context 'which returns a gif' do
       Codeship::Status::STATES.each do |state|
         it "should parse #{state}" do
+
           stub_request(:head, "https://app.codeship.com/projects/#{state}/status?branch=master").
                    with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
                    to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.gif\""})
+
+          project_status = Codeship::Status.new(state, branch: 'master')
+          expect(project_status.status).to eq(state)
+        end
+      end
+    end
+
+    context 'which returns a svg' do
+      Codeship::Status::STATES.each do |state|
+        it "should parse #{state}" do
+
+          stub_request(:head, "https://app.codeship.com/projects/#{state}/status?branch=master").
+                   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+                   to_return(status: 200, body: "", headers: {'Content-Disposition' => "inline; filename=\"status_#{state}.svg\""})
 
           project_status = Codeship::Status.new(state, branch: 'master')
           expect(project_status.status).to eq(state)
